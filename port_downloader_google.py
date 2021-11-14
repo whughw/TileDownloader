29f25d08b8d2ad4bb1e1af4c4c0f83f0bafbdcd3
whughw <1@whughw.com>
2021-11-14 14:22:16 +0800

    add arguments

--- port_downloader_google.py
+++ port_downloader_google.py
@@ -6,6 +6,7 @@ import csv
 
 import tile_downloader
 import geo_utils
+from argparse import ArgumentParser
 
 dlng = 2
 dlat = 2
@@ -18,25 +19,33 @@ with open("port.csv", mode="r", encoding="utf-8") as f:
     # loc_list = [[l[2], float(l[22]), float(l[23])] for l in loc_list]
     loc_list = [[float(l[1]), float(l[0])] for l in loc_list]
 
-datasource = [
-              {'datasource': 'google', 'zoom': 19},
-              # {'datasource': 'bing', 'zoom': 19},
-              # {'datasource': 'arcgis', 'zoom': 19}
-              ]
+parser = ArgumentParser()
+parser.add_argument(
+    '--source', type=str, default=r"google")
+parser.add_argument(
+    '--start', type=int, default=0)
+parser.add_argument(
+    '--end', type=int, default=len(loc_list))
+args = parser.parse_args()
 
-start_idx = 0
-end_idx = 500
+datasource = {
+              "google": {'datasource': 'google', 'zoom': 19},
+              "bing": {'datasource': 'bing', 'zoom': 19},
+              "arcgis": {'datasource': 'arcgis', 'zoom': 19}
+}
 
-# for idx in range(resume_idx, len(loc_list)):
+start_idx = args.start
+end_idx = args.end
+
 for idx in range(start_idx, end_idx):
     try:
         loc = loc_list[idx]
         print("{} / {}".format(idx, len(loc_list)))
         lng, lat = loc[1], loc[0]
-        for d in datasource:
-            image = tile_downloader.get_poi(lng, lat, dlng=dlng, dlat=dlat, **d)
-            cv2.imencode('.jpg', image)[1].tofile\
-                (os.path.join(save_path, "{}_{}.jpg".format(str(idx), d['datasource'])))
+        d = datasource[args.source]
+        image = tile_downloader.get_poi(lng, lat, dlng=dlng, dlat=dlat, **d)
+        cv2.imencode('.jpg', image)[1].tofile\
+            (os.path.join(save_path, "{}_{}.jpg".format(str(idx), d['datasource'])))
     except Exception as e:
         print(str(e))
         continue
